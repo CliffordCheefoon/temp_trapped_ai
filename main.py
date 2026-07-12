7fe3de7ed674d6432fd2b5c0772b1b9f40e91dd9
Clifford Cheefoon <cheefoonclifford@yahoo.com>
2026-07-11 21:14:43 -0400

    add content

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from RPLCD.i2c import CharLCD
 
-lcd = CharLCD('PCF8574', 0x27)
+lcd = CharLCD('PCF8574', 0x27, cols=40, rows=4)
 
-lcd.write_string('Hello, World! 1')
-lcd.write_string('Hello, World! 2')
-lcd.write_string('Hello, World! 3')
-lcd.write_string('Hello, World! 4')
+lcd.write_string('Hello, World 1!\n') 
+
+lcd.write_string('Hello, World 2!\n') 
+
+lcd.write_string('Hello, World 3!\n') 
+
+lcd.write_string('Hello, World 4!\n') 
