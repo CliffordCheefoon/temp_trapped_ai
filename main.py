f899d83f4af168f19c150bd5322ba8293b507c43
Clifford Cheefoon <cheefoonclifford@yahoo.com>
2026-07-11 21:11:39 -0400

    more strings

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from RPLCD.i2c import CharLCD
 
 lcd = CharLCD('PCF8574', 0x27)
 
-lcd.write_string('Hello, World!')
+lcd.write_string('Hello, World! 1')
+lcd.write_string('Hello, World! 2')
+lcd.write_string('Hello, World! 3')
+lcd.write_string('Hello, World! 4')
