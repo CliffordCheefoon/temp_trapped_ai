f35d63dca5c009487f9c52419643247bbe812316
Clifford Cheefoon <cheefoonclifford@yahoo.com>
2026-07-11 23:16:50 -0400

    new lib

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from RPLCD.i2c import CharLCD
 
 # lcd = CharLCD('PCF8574', 0x27, cols=40, rows=4)
+# lcd.backlight_enabled = True
 
 # lcd.write_string('Hello, World 1!\n') 
 
@@ -10,12 +11,19 @@
 
 # lcd.write_string('Hello, World 4!\n') 
 
-from charlcd import direct as lcd
-from charlcd.drivers.i2c import I2C
 
+## https://bitbucket.org/kosci/charlcd/src/master/
+from charlcd import buffered as lcd
+from charlcd.drivers.i2cm import I2C as I2CM
 
-l = lcd.CharLCD(40, 4, I2C(0x27, 1))
-l.write("Hello, World 1!, Hello, World 2!, Hello, World 3!, Hello, World 4!, Hello, World 5!, Hello, World 6!, Hello, World 7!, Hello, World 8!, Hello, World 9!, Hello, World 10!, Hello, World 11!, Hello, World 12!, Hello, World 13!, Hello, World 14!, Hello, World 15!, Hello, World 16!, Hello, World 17!, Hello, World 18!, Hello, World 19!, Hello, World 20!")
+i2cm_interface = I2CM(0x27, 1)
+i2cm_interface.pins['E2'] = 1
+
+lcd_writer = lcd.CharLCD(40, 4, i2cm_interface, 0, 0)
+
+lcd_writer.init()
+lcd_writer.stream('Line 1\nLine 2\nLine 3\nLine 4\n')
+lcd_writer.flush()
 
 
 
