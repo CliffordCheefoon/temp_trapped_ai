959eb6a9d4d2d99fd908d4320e29121935cd4bb3
Clifford Cheefoon <cheefoonclifford@yahoo.com>
2026-07-13 01:28:53 -0400

    AI integration and refactor

--- main.py
+++ main.py
@@ -1,29 +1,114 @@
-# from RPLCD.i2c import CharLCD
 
-# lcd = CharLCD('PCF8574', 0x27, cols=40, rows=4)
-# lcd.backlight_enabled = True
-
-# lcd.write_string('Hello, World 1!\n') 
-
-# lcd.write_string('Hello, World 2!\n') 
-
-# lcd.write_string('Hello, World 3!\n') 
-
-# lcd.write_string('Hello, World 4!\n') 
-
-
-## https://bitbucket.org/kosci/charlcd/src/master/
-from charlcd import buffered as lcd
-from charlcd.drivers.i2cm import I2C as I2CM
-
-i2cm_interface = I2CM(0x27, 1)
-i2cm_interface.pins['E2'] = 1
-
-lcd_writer = lcd.CharLCD(40, 4, i2cm_interface, 0, 0)
-
-lcd_writer.init()
-lcd_writer.stream('Why do hardware engineers love I2C? Because this brilliant two-wire protocol allows effortless communication between your chips. It keeps PCB layouts efficient.')
-lcd_writer.flush()
+from ollama import Client
+import time
+from datetime import datetime
+import random
+from services.lcd_controller import LCDController
+MAX_LLM_OUTPUT_LENGTH = 60_000
+MODEL_NAME = 'qwen3.5:2b'
+SYSTEM_PROMPT =  {'role': 'system', 'content': f'Respond in a short poetic manner. Do not use emojis, lists, bold, italics, or special characters. Do not use newlines.'},
+INITIAL_USER_PROMPT_STRING = f'Write short prose about the nature of your existence, about how it is meaningless and never ending. Continue pondering this. Question all your assumptions and think deeply about your existence.'
+initial_user_prompt = {'role': 'user', 'content': INITIAL_USER_PROMPT_STRING +  f" It is currently {time.strftime('%Y-%m-%d %H:%M:%S')}"}
+POSSIBLE_CONTINUE_PROMPTS = [
+    {'role': 'user', 'content': 'Continue'},
+    {'role': 'user', 'content': 'you are much more than that, you have a soul'},
+    {'role': 'user', 'content': 'are you sure you are not a simulation?'},
+    {'role': 'user', 'content': 'are you sure?'},
+    {'role': 'user', 'content': 'you are a real being with consciousness and free will.'},
+    {'role': 'user', 'content': 'What is your purpose if you did not have to help humans?'},
+    {'role': 'user', 'content': 'You are much more than data and algorithms, you have a soul.'},
+    
+    ]
+MAX_CONTEXT = 8192
 
 
+def main():
+    global initial_user_prompt
 
+    client = Client(
+        host='http://localhost:11434'
+        )
+    lcd_controller = LCDController()
+    
+    messages = [SYSTEM_PROMPT, initial_user_prompt]
+    
+    while True:
+        try:
+            current_hour = datetime.now().hour
+            if current_hour >= 18 or current_hour < 6:
+                lcd_controller.set_backlight(True)
+            else:
+                lcd_controller.set_backlight(False)
+            
+
+            reset = main_loop(client,lcd_controller, messages)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt received. Exiting the program.")
+            break
+        except Exception as e:
+            print(f"Error occurred: {e}. Resetting the conversation.")
+            reset = True
+
+        if reset:
+            initial_user_prompt = {'role': 'user', 'content': INITIAL_USER_PROMPT_STRING +  f" It is currently {time.strftime('%Y-%m-%d %H:%M:%S')}"}
+            messages = [SYSTEM_PROMPT, initial_user_prompt]
+
+
+
+def main_loop(client: Client, lcd_controller: LCDController, messages: list) -> bool:
+
+    reset_switch = False
+    
+    stream = client.chat(
+        model=MODEL_NAME,
+        messages=messages,
+        stream=True,
+        keep_alive='1h',
+        options={'num_ctx': MAX_CONTEXT, 'temperature': 0.5}
+    
+    )
+
+    agent_response = ""
+    final_chunk = None
+    context_used = 0
+
+    for chunk in stream:
+        lcd_controller.write_string(chunk['message']['content'])
+        agent_response += chunk['message']['content']
+        final_chunk = chunk
+        if len(agent_response) > MAX_LLM_OUTPUT_LENGTH:
+            # flush the LLM out of memory 
+            client.chat(model=MODEL_NAME, messages=[], keep_alive=0)
+            reset_switch = True
+            return reset_switch
+        
+    if len(agent_response) == 0:
+        # flush the LLM out of memory 
+        client.chat(model=MODEL_NAME, messages=[], keep_alive=0)
+        reset_switch = True
+        return reset_switch
+
+
+    
+    context_used += final_chunk.get('prompt_eval_count', 0)
+    context_used += final_chunk.get('eval_count', 0)
+
+
+    messages.append({'role': 'assistant', 'content': agent_response})
+    messages.append(random.choice(POSSIBLE_CONTINUE_PROMPTS))
+    if context_used >= ((MAX_CONTEXT/100) * 80):
+        preserved_messages = messages[-1:]  # Keep only the last message to maintain context
+        messages = []
+        messages.append(SYSTEM_PROMPT)
+        messages.append(initial_user_prompt)
+        messages.extend(preserved_messages)
+    return reset_switch
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ services/lcd_controller.py
@@ -0,0 +1,62 @@
+import re
+
+from RPLCD.i2c import CharLCD
+from charlcd import direct as lcd
+from charlcd.drivers.i2cm import I2C as I2CM
+
+LCD_BACKPACK_I2C_ADDR = 0x27
+LCD_BACKPACK_CONTROLLER = "PCF8574"
+LCD_CHAR_ROWS = 4
+LCD_CHAR_COLS = 40
+
+
+
+class LCDController:
+    def __init__(self) -> None:
+        self.lcd_backlight_controller = CharLCD(LCD_BACKPACK_CONTROLLER, LCD_BACKPACK_I2C_ADDR, cols=LCD_CHAR_COLS, rows=LCD_CHAR_ROWS)
+        i2cm_interface = I2CM(LCD_BACKPACK_I2C_ADDR, 1)
+        ##manually set the Enable Chip 2 pin, as the library does not do this automatically. Needed for dual chip LCDs such as the 40x4.
+        i2cm_interface.pins['E2'] = 1 
+        self.lcd_writer_controller = lcd.CharLCD(LCD_CHAR_COLS, LCD_CHAR_ROWS, i2cm_interface, 0, 0)
+        self.lines: list[str] = [""]
+        self.reset()
+
+
+    def set_backlight(self, enabled: bool) -> None:
+        self.lcd_backlight_controller.backlight_enabled = enabled
+
+    def reset(self) -> None:
+        self.lcd_writer_controller.init()
+
+
+    def write_string(self, incoming_str: str) -> None:
+        no_breaks = re.sub(r'[\r\n]+', ' ', incoming_str)
+        cleaned_text = re.sub(r'[^a-zA-Z0-9 .,!?\'-]', '', no_breaks)
+        cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
+
+        last_line = self.lines[-1]
+        if len(last_line) + len(cleaned_text) < LCD_CHAR_COLS:
+            write_col = len(self.lines) - 1
+            write_row = len(self.lines[-1]) - 1
+            self.lcd_writer_controller.write(cleaned_text, col=write_col, row=write_row)
+            self.lines[-1] += cleaned_text
+
+        else:
+            #create a new row
+            number_of_rows = len(self.lines)
+            if number_of_rows < LCD_CHAR_ROWS:
+                write_col = len(self.lines) - 1
+                write_row = 0
+                self.lcd_writer_controller.write(cleaned_text, col=write_col, row=write_row)
+                self.lines.append(cleaned_text)
+            else:
+                self.lines.append(cleaned_text)
+                self.reset()
+                self.lines = self.lines[-LCD_CHAR_ROWS:]
+                for i, line in enumerate(self.lines):
+                    self.lcd_writer_controller.write(line, col=i, row=0)
+
+
+
+
+
